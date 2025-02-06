41dbb0f0427123313fe8ed8abd0c616e5b1a47b0
jmoreno1852 <javiermmvv02@gmail.com>
2025-02-06 17:11:45 +0100

    Implementation of main.py and server.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-
-
+import threading
+from src.server import *
 
 def main()
-clients = []
-nicknames = []
+    clients = []    #Global variable to save client connections
+    nicknames = []  #Global variable to save client names
+    host = "127.0.0.1"
+    port = 4444
+
+    s = setup_server(host, port
+    try:
+        handle_client(s)
+    except KeyboardInterrupt:
+        close_server(s)
+        
 if __name__ == "__init__":
     main()
--- src/server.py
+++ src/server.py
@@ -3,31 +3,73 @@ import threading
 import sys
 
 #Funciones necesarias
-#Setear el servidor
 #Manejar a los clientes
-#Broadcast de los mensajes
-#Eliminar conexiones 
-
+    #Recibir las conexiones de los clientes
+    #Guardar las conexiones en la variable global clientes
+    #Guardas el Nombre de cada una de las conexiones 
+    #Cuando se envia un mensaje realizar un broadcast del mensaje del cliente
+    #Mostrar en la CLI de los otros clientes el nombre de quien ha envicado el mensaje
+    
 #Necesito una lista de clientes
+
+
+#Set server - Done
 def setup_server(host,port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host,port))
     s.listen(5)
-    print(f"Server listentint on {host}:{port}")
+    print(f"Server listening on {host}:{port}")
+    return s
 
-def handle_client(client):
+#Manage Clients, accept client connection and save its name
+def handle_client(s):
+    global clients
+    global nicknames
     while True:
-        data = client.recv(1024).decode('utf-8')
-        if  not data:
+        #Accept clients connection and save it
+        client, addr = s.accept()
+        clients.append(client)
+        print(f"Connection form {addr}")
+        
+        #Store Nickname 
+        client.send('Name:'.encode("utf-8"))
+        nickname = client.recv(1024).decode("utf-8")     
+        nicknames.append(nickname)
+        client.send("You are now connected.".encode('utf-8'))
+        #Handle client thread    
+        thread = threading.Thread(target=receive_data,args=(client))
+        thread.start()
+        
+def receive_data(client):
+    global clients
+    while True:
+        try:  
+            data = client.recv(1024).decode('utf-8')
+            if  not data:
+                break
+            broadcast(data, client)
+            print(f"\nData recieved: {data}")
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nickname[index]
+            broadcast('{} left!'.format(nickname).encode('utf-8'),client)
+            nicknames.remove(nickname)
             break
-        print(f"\nData recieved: {data}")
+            
     client.close()
+    remove(client)
 
+
+#Function to remove client from global variable clients
 def remove(client):
+    global clients
     if client in clients:
         clients.remove(client)
 
-def broadcast(data, connection, clients):
+def broadcast(data, connection):
+    global clients
     for client in clients:
         if client != connection:
             try:
@@ -36,3 +78,10 @@ def broadcast(data, connection, clients):
                 client.close()
                 remove(client)
 
+#Asuring to close al clients before closing the server
+def close_server(s):
+    global clients
+    for client in clients:
+        client.close()
+    s.close()
+    print("Clients and Server closed")
