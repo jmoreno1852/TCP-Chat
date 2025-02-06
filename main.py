c7c436139282687b25d87ead5ae5cf05d5acc0f8
jmoreno1852 <javiermmvv02@gmail.com>
2025-02-06 17:31:34 +0100

    Minor fixes

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import threading
 from src.server import *
 
-def main()
+def main():
     clients = []    #Global variable to save client connections
     nicknames = []  #Global variable to save client names
     host = "127.0.0.1"
     port = 4444
 
-    s = setup_server(host, port
+    s = setup_server(host, port)
     try:
         handle_client(s)
     except KeyboardInterrupt:
         close_server(s)
         
-if __name__ == "__init__":
+if __name__ == "__main__":
     main()
